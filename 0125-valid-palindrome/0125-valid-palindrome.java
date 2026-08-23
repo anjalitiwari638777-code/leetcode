2d533d27a912572517039e3d11667ad07ca41217
Anjali Tiwari <anjalitiwari638777@gmail.com>
2026-08-23 12:01:40 +0530

    Time: 13 ms (31.84%), Space: 46 MB (33.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
-        s=s.replaceAll("[^a-z0-9]","");
+        s = s.replaceAll("[^a-z0-9]","");
         int left = 0;
         int right = s.length()-1;
         while(left<right){
             if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
-            left++;
-            right--;
+                left++;
+                right--;
+            
         }
         return true;
+        
     }
 }
-        
