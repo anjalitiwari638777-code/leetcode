bd88ac224cf9eb0060d6f3b0fe739be4ddadce19
Anjali Tiwari <anjalitiwari638777@gmail.com>
2026-08-23 11:53:00 +0530

    Time: 2 ms (95.59%), Space: 48.3 MB (61.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -5,15 +5,14 @@ class Solution {
         while(left<right){
             int sum = numbers[left]+numbers[right];
             if(sum==target){
-                return new int[]{left+1,right+1};
+                return new int[] {left+1,right+1};
             }else if(sum<target){
                 left++;
             }else{
                 right--;
             }
         }
-        return new int[]{};
-       
+        return new int[] {};
         
     }
 }
