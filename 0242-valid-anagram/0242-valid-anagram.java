f570edffe0478a72d5222db0bc2b0e2679903ef5
Anjali Tiwari <anjalitiwari638777@gmail.com>
2026-09-06 17:27:44 +0530

    Time: 5 ms (62.9%), Space: 44.2 MB (89.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,34 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
+        if(s.length()!=t.length()){
             return false;
         }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // Count characters of s
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
         }
-
-        // Subtract characters of t
-        for (char c : t.toCharArray()) {
-
-            if (!map.containsKey(c)) {
-                return false;
-            }
-
-            map.put(c, map.get(c) - 1);
-        }
-
-        // Check all frequencies
-        for (int count : map.values()) {
-            if (count != 0) {
+        for(int count:freq){
+            if(count!=0){
                 return false;
             }
         }
-
         return true;
+        
     }
 }
